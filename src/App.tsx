75d792bc685ab523668e885d2cfaa582c11fdccf
alona <alionazhv@gmail.com>
2024-04-18 13:11:11 +0200

    The react routing was implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import './App.scss';
 import { HomePage } from './Components/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Phones } from './Components/Phones';
 
 export const App = () => {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/phones" element={<Phones />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
