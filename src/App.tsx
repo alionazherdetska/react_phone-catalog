2064737ae852f2358c6bb52d99409f5266ec9fe6
alona <alionazhv@gmail.com>
2024-04-22 09:58:56 +0200

    The new routes were added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.scss';
 import { HomePage } from './Components/HomePage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Phones } from './Components/Phones';
+import { ItemCard } from './Components/ItemCard';
+import { Favorites } from './Components/Favorites';
+import { ShoppingCart } from './Components/ShoppingCart';
 
 export const App = () => {
   return (
@@ -9,6 +12,9 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/phones" element={<Phones />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        <Route path="/item-card" element={<ItemCard />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </BrowserRouter>
   );
