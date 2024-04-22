1b409508229dd28bf131c7f58624486479be9301
alona <alionazhv@gmail.com>
2024-04-22 10:00:20 +0200

    The changes to Phones  were made

--- src/Components/Phones/Phones.tsx
+++ src/Components/Phones/Phones.tsx
@@ -5,34 +5,63 @@ import { ProductCard } from '../ProductCard';
 
 const Phones: React.FC = () => {
   return (
-    <>
+    <div className="phones">
       <Header />
-      <main className="phones">
-        <section className="phones__top">
-          <img src="" />
-          <img src="" />
-          <h4>Phones</h4>
-          <h1>Mobile phones</h1>
-          <p>95 models</p>
-        </section>
-        <section className="phones__main">
-          <div className="phones__main__sorting">
-            <select className="phones__main__sorting--by">
-              <option>Newest</option>
-            </select>
-            <select className="phones__main__sorting--by">
-              <option>16</option>
-            </select>
+
+      <section className="phones__top">
+        <div className="phones__top--search-params">
+          <div className="phones__top--search-params__icons">
+            <img
+              src="/icons/home.svg"
+              className="phones__top--search-params__icons-home"
+              alt="Home"
+            />
+            <img
+              src="/icons/slider_gray.svg"
+              className="phones__top--search-params__icons-slider"
+              alt="Slider"
+            />
           </div>
-        </section>
-        <ul className="phones__list">
-          <div className="phones__list__product-card">
-            <ProductCard />
-          </div>
+          <h4 className="phones__top--search-params__name">Phones</h4>
+        </div>
+        <h1>Mobile phones</h1>
+        <p>95 models</p>
+      </section>
+      <section className="phones__main">
+        <div className="phones__main__sorting-by">
+          <label htmlFor="sortingBy">Sort by</label>
+          <select id="sortingBy">
+            <option className="phones__main__sorting-by__option">Newest</option>
+          </select>
+        </div>
+        <div className="phones__main__amount">
+          <label htmlFor="perPage">Items on page</label>
+          <select id="perPage">
+            <option>16</option>
+          </select>
+        </div>
+      </section>
+      <ul className="phones__list">
+        <div className="phones__list__product-card">
+          <ProductCard />
+        </div>
+      </ul>
+      <ul className="phones--slider">
+        <li className="phones--slider--layout">
+          <img src="/icons/slider_gray.svg" alt="Slider to the left" />
+        </li>
+        <ul className="phones--slider--pages">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
         </ul>
-      </main>
+        <li className="phones--slider--layout">
+          <img src="/icons/slider_black.svg" alt="Slider to the right" />
+        </li>
+      </ul>
       <Footer />
-    </>
+    </div>
   );
 };
 
