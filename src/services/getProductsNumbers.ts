9e6187b1869395bb212a3d11dcfb52ce08f46459
alona <alionazhv@gmail.com>
2024-05-04 15:48:45 +0200

    The services were added

---
+++ src/services/getProductsNumbers.ts
@@ -0,0 +1,9 @@
+export function getProductsNumber(from: number, to: number): number[] {
+  const numbers = [];
+
+  for (let n = from; n <= to; n += 1) {
+    numbers.push(n);
+  }
+
+  return numbers;
+}
