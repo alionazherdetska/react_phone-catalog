5898dbcb8b7e43e790f4c52ee2a632589af7d87b
alona <alionazhv@gmail.com>
2024-05-09 09:15:38 +0200

    The base url was fixed

--- src/services/fetchClients.ts
+++ src/services/fetchClients.ts
@@ -1,7 +1,7 @@
 import Product from '../types/types';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const BASE_URL = '/api';
+const BASE_URL = '/react_phone-catalog/api';
 
 // returns a promise resolved after a given delay
 function wait(delay: number) {
