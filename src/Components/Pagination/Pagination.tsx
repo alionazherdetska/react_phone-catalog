9e5c1d9a9b91e4b2118bffb4318721e60c0c2f2b
alona <alionazhv@gmail.com>
2024-05-09 08:35:14 +0200

    Displaying pages in pagination component was adjusted

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -1,5 +1,6 @@
 import gray_slider from '../../assets/icons/slider_gray.svg';
 import black_slider from '../../assets/icons/slider_black.svg';
+import { useEffect, useState } from 'react';
 
 interface PaginationProps {
   totalItems: number;
@@ -13,52 +14,63 @@ const Pagination: React.FC<PaginationProps> = ({
   currentPage = 1,
   onPageChange,
 }) => {
-  const totalPages = Math.ceil(totalItems / perPage);
+  const [totalPages, setTotalPages] = useState(Math.ceil(totalItems / perPage));
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(totalItems / perPage));
+  }, [totalItems, perPage]);
+
   const handlePageClick = (page: number) => {
     onPageChange(page);
   };
 
-  const pageNumbers: number[] = [];
+  const generatePageNumbers = () => {
+    const range = 4;
+    const start = Math.max(1, currentPage - range);
+    const end = Math.min(totalPages, currentPage + range);
 
-  for (let i = 1; i <= totalPages; i++) {
-    pageNumbers.push(i);
-  }
+    const pageNumbers: number[] = [];
+
+    for (let i = start; i <= end; i++) {
+      pageNumbers.push(i);
+    }
+
+    return pageNumbers;
+  };
 
   return (
-    <ul className="phones--slider">
-      <li className="phones--slider--layout">
-        <button
-          onClick={() => handlePageClick(currentPage - 1)}
-          disabled={currentPage === 1}
-          aria-label="Previous Page"
-        >
-          <img src={gray_slider} alt="Slider to the left" />
-        </button>
-      </li>
-      <ul className="phones--slider--pages">
-        {pageNumbers.map(page => (
-          <li key={page}>
-            <button
-              className={
-                currentPage === page ? 'page-link active' : 'page-link'
-              }
-              onClick={() => handlePageClick(page)}
-            >
-              {page}
-            </button>
-          </li>
+    <ul className="pagination--slider">
+      <button
+        className="pagination--slider--layout"
+        onClick={() => handlePageClick(currentPage - 1)}
+        disabled={currentPage === 1}
+        aria-label="Previous Page"
+      >
+        <img src={gray_slider} alt="Slider to the left" />
+      </button>
+      <ul className="pagination--slider--pages">
+        {generatePageNumbers().map(page => (
+          <button
+            key={page}
+            className={
+              currentPage === page
+                ? 'pagination--slider--layout--active'
+                : 'pagination--slider--layout'
+            }
+            onClick={() => handlePageClick(page)}
+          >
+            {page}
+          </button>
         ))}
       </ul>
-
-      <li className="phones--slider--layout">
-        <button
-          onClick={() => handlePageClick(currentPage + 1)}
-          disabled={currentPage === totalPages}
-          aria-label="Next Page"
-        >
-          <img src={black_slider} alt="Slider to the right" />
-        </button>
-      </li>
+      <button
+        className="pagination--slider--layout"
+        onClick={() => handlePageClick(currentPage + 1)}
+        disabled={currentPage === totalPages}
+        aria-label="Next Page"
+      >
+        <img src={black_slider} alt="Slider to the right" />
+      </button>
     </ul>
   );
 };
